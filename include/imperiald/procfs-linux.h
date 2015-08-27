f4e670cf50f8f0d9f7cf621753fa9a584e48eee1
Magnus Deininger <magnus@ef.gy>
2015-08-27 12:51:18 -0400

    grab more interesting metrics from /proc/stat

--- include/imperiald/procfs-linux.h
+++ include/imperiald/procfs-linux.h
@@ -32,6 +32,8 @@
 #include <prometheus/metric.h>
 #include <fstream>
 #include <regex>
+#include <thread>
+#include <chrono>
 
 namespace imperiald {
 namespace linux {
@@ -46,35 +48,106 @@ public:
     : file(pFile),
       prometheus::collector::hub(prefix + "linux_procfs_stats", pRegistry,
       "hub"),
-      bootTime(prefix + "boot_time_seconds", pRegistry)
+      bootTime(prefix + "boot_time_seconds", pRegistry),
+      contextSwitches(prefix + "context_switches_total", pRegistry),
+      interrupts(prefix + "interrupts_total", pRegistry),
+      processes(prefix + "forks_total", pRegistry),
+      runningProcesses(prefix + "running_processes", pRegistry),
+      blockedProcesses(prefix + "blocked_processes", pRegistry),
+      userCPUTime(prefix + "cpu_user_user_hz", pRegistry),
+      niceCPUTime(prefix + "cpu_nice_user_hz", pRegistry),
+      systemCPUTime(prefix + "cpu_system_user_hz", pRegistry),
+      idleCPUTime(prefix + "cpu_idle_user_hz", pRegistry),
+      pageIn(prefix + "page_in_pages", pRegistry),
+      pageOut(prefix + "page_out_pages", pRegistry),
+      swapIn(prefix + "swap_in_pages", pRegistry),
+      swapOut(prefix + "swap_out_pages", pRegistry),
+      stop(false)
     {
-      update();
+      updateThread = std::thread([this, updateInterval]() {
+        while(!stop) {
+          update();
+          std::this_thread::sleep_for(std::chrono::seconds(updateInterval));
+        }
+      });
     }
 
+  ~stat() {
+    stop = true;
+    updateThread.join();
+  }
+
+  static T asNumber(const std::string &s) {
+    std::istringstream iss(s);
+    T bt;
+
+    iss >> bt;
+    return bt;
+  }
+
+protected:
   bool update (void) {
     std::ifstream i(file);
     std::string line;
 
     while (std::getline(i, line)) {
       static std::regex btime("btime ([0-9]+).*");
+      static std::regex ctxt("ctxt ([0-9]+).*");
+      static std::regex intr("intr ([0-9]+).*");
+      static std::regex procs("processes ([0-9]+).*");
+      static std::regex procs_running("procs_running ([0-9]+).*");
+      static std::regex procs_blocked("procs_blocked ([0-9]+).*");
+      static std::regex cpu("cpu\\s+([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+).*");
+      static std::regex page("page ([0-9]+) ([0-9]+).*");
+      static std::regex swap("swap ([0-9]+) ([0-9]+).*");
       std::smatch matches;
 
       if (std::regex_match(line, matches, btime)) {
-        std::istringstream iss(matches[1]);
-        T bt;
-
-        iss >> bt;
-
-        bootTime.set(bt);
+        bootTime.set(asNumber(matches[1]));
+      } else if (std::regex_match(line, matches, ctxt)) {
+        contextSwitches.set(asNumber(matches[1]));
+      } else if (std::regex_match(line, matches, intr)) {
+        interrupts.set(asNumber(matches[1]));
+      } else if (std::regex_match(line, matches, procs)) {
+        processes.set(asNumber(matches[1]));
+      } else if (std::regex_match(line, matches, procs_running)) {
+        runningProcesses.set(asNumber(matches[1]));
+      } else if (std::regex_match(line, matches, procs_blocked)) {
+        blockedProcesses.set(asNumber(matches[1]));
+      } else if (std::regex_match(line, matches, cpu)) {
+        userCPUTime.set(asNumber(matches[1]));
+        niceCPUTime.set(asNumber(matches[2]));
+        systemCPUTime.set(asNumber(matches[3]));
+        idleCPUTime.set(asNumber(matches[4]));
+      } else if (std::regex_match(line, matches, page)) {
+        pageIn.set(asNumber(matches[1]));
+        pageOut.set(asNumber(matches[2]));
+      } else if (std::regex_match(line, matches, swap)) {
+        swapIn.set(asNumber(matches[1]));
+        swapOut.set(asNumber(matches[2]));
       }
     }
 
     return true;
   }
 
-protected:
+  bool stop;
   const std::string file;
-  prometheus::metric::gauge<T> bootTime;
+  prometheus::metric::counter<T> bootTime;
+  prometheus::metric::counter<T> contextSwitches;
+  prometheus::metric::counter<T> interrupts;
+  prometheus::metric::counter<T> processes;
+  prometheus::metric::gauge<T> runningProcesses;
+  prometheus::metric::gauge<T> blockedProcesses;
+  prometheus::metric::counter<T> userCPUTime;
+  prometheus::metric::counter<T> niceCPUTime;
+  prometheus::metric::counter<T> systemCPUTime;
+  prometheus::metric::counter<T> idleCPUTime;
+  prometheus::metric::gauge<T> pageIn;
+  prometheus::metric::gauge<T> pageOut;
+  prometheus::metric::gauge<T> swapIn;
+  prometheus::metric::gauge<T> swapOut;
+  std::thread updateThread;
 };
 }
 }
